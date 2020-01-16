05f8fe333de1aae019a40db1c1527a250fb822dc
VladimirZhd <zhd18001@byui.edu>
2020-01-15 22:47:36 -0700

    removed

--- mobile-view-test/src/js/functions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".button-menu").click(function(){
-        $(".hide").animate({left: "0"});
-    });
-    $(".button-close").click(function(){
-        $(".hide").animate({left: "-340px"});
-    })
-});
