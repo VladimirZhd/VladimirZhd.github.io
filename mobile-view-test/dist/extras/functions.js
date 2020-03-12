dfd5bc8d6a74812e98c6d8f3b60ded4b665558f9
VladimirZhd <zhd18001@byui.edu>
2020-03-12 07:45:02 -0600

    up

--- mobile-view-test/dist/extras/functions.js
+++ mobile-view-test/dist/extras/functions.js
@@ -88,28 +88,3 @@ function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
 
-// const eventContract = new jsaction.EventContract();
-
-// eventContract.addContainer(document.getElementById('near-mobile'));
-
-// eventContract.addEvent('pointerleave');
-// console.log(eventContract);
-
-// const dispatcher = new new jsaction.Dispatcher();
-// eventContract.dispatchTo(dispatcher.dispatch.bind(dispatcher));
-
-// const hideMenu = function(flow) {
-//     let menu = document.getElementById('near-mobile');
-//     menu.style.transition = 'none 0s ease 0s';
-//     menu.style.willChange = 'transform';
-//     menu.style.transform = 'matrix(1, 0, 0, 1, 0, -410) translateY(0px)';
-// }
-
-// dispatcher.registerHandlers(
-//     'start',
-//     null,
-//     {
-//         'drag': hideMenu
-//     }
-// );
-
