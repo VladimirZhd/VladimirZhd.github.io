2a27405789364eba815c98eff7ad397fc894db21
VladimirZhd <zhd18001@byui.edu>
2020-09-19 14:31:15 -0600

    up

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,83 @@
 const TypeWriter = function (txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
 
 //Type Method
-TypeWriter.prototype.type = function() {
-    // Current index of word
-    const current = this.wordIndex % this.words.length;
-    // Get full text of current word
-    const fullTxt = this.words[current];
+TypeWriter.prototype.type = function () {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  // Get full text of current word
+  const fullTxt = this.words[current];
 
-    // Check if deleting
-    if (this.isDeleting) {
-        // Remove a char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        // Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  // Check if deleting
+  if (this.isDeleting) {
+    // Remove a char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-    // Insert Txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  // Insert Txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    // Initial Type speed
-    let typeSpeed = 200;
+  // Initial Type speed
+  let typeSpeed = 200;
 
-    if (this.isDeleting) {
-        typeSpeed /= 2;
-    }
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
 
-    // Check if the word is complete
-    if (!this.isDeleting && this.txt === fullTxt) {
-        // Make a pause at end
-        typeSpeed = this.wait;
-        // Set deleting to true
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        // Move to next word
-        this.wordIndex++;
-        // Pause before start typing
-        typeSpeed = 500;
-    }
+  // Check if the word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // Make a pause at end
+    typeSpeed = this.wait;
+    // Set deleting to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    // Move to next word
+    this.wordIndex++;
+    // Pause before start typing
+    typeSpeed = 500;
+  }
 
-    setTimeout(() => this.type(), typeSpeed);
-}
+  setTimeout(() => this.type(), typeSpeed);
+};
 
 // Init on DOM load
 document.addEventListener('DOMContentLoaded', init);
 
 // Init App
 function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
-    new TypeWriter(txtElement, words, wait);
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
 }
+
+const links = [
+  {
+    label: 'Week 1 Notes',
+    url: '/portfoliowdd330/week1/index.html',
+  },
+];
+
+function addNotes() {
+  links.forEach((item) => {
+    const liContent = `<a href='${item.url}'>${item.label}</a>`;
+    const li = document.createElement('li');
+    li.innerHTML = liContent;
+    const ol = document.getElementById('list');
+    ol.appendChild(li);
+  });
+}
+
+addNotes();
