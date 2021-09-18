7223722bb9880eb6e5f2d19fd1674f7d3736f52f
VladimirZhd <zhd18001@byui.edu>
2021-09-17 23:51:34 -0600

    updates to the project

--- portfolioWdd330/class-work/week10/main.js
+++
@@ -1,5 +0,0 @@
-import { search } from './search.js';
-
-const queryBox = document.getElementById('search');
-
-queryBox.addEventListener('keyup', search);
--- portfolioWdd330/team-activities/week10/js/index.js
+++
@@ -1,5 +0,0 @@
-import QuakesController from './QuakesController.js';
-
-const qc = new QuakesController('#quakeList');
-
-qc.init();
